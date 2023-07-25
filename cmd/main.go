89378a85bb46f0f70370a6070716de3c5811fc68
WalkerChel <illiakazlou28@gmail.com>
2023-07-25 16:10:32 +0300

    Creating methods for Items, Lists and Auth

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	todo "github.com/WalkerChel/TO-DO"
+	"github.com/WalkerChel/TO-DO/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
