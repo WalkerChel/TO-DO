b58dc15a01c8c7771add6b78d9577d1ec98c957c
WalkerChel <illiakazlou28@gmail.com>
2023-07-26 16:12:29 +0300

    Dependency Injection implementation: handler->service->repository

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	todo "github.com/WalkerChel/TO-DO"
 	"github.com/WalkerChel/TO-DO/pkg/handler"
+	"github.com/WalkerChel/TO-DO/pkg/repository"
+	"github.com/WalkerChel/TO-DO/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	service := service.NewService(repos)
+	handlers := handler.NewHandler(service)
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
