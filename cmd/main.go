5c341e15121a3694ac25dd494392c0cf26eb4036
WalkerChel <illiakazlou28@gmail.com>
2023-07-27 17:50:59 +0300

    Create PSQL DB via docker and creatint file config

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,28 @@ import (
 	"github.com/WalkerChel/TO-DO/pkg/handler"
 	"github.com/WalkerChel/TO-DO/pkg/repository"
 	"github.com/WalkerChel/TO-DO/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	service := service.NewService(repos)
 	handlers := handler.NewHandler(service)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
